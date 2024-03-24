a546d2a497cb346c1829523d02bede2ca0eca3e4
Vinicius Araujo <araujoviine@gmail.com>
2024-03-24 20:22:10 -0300

    atualizei o styling do front, adicionei lemmatization para o user input - sendo necessario criar um script adicional para 'handle' spacy lib and its usage

--- front_app.py
+++ front_app.py
@@ -1,58 +1,19 @@
 import streamlit as st
-from gcp import predict
+from gcp import get_top_confidence_category
+import setup_spacy as ss
+from spacy.lang.pt.stop_words import STOP_WORDS
 
+nlp = ss.download_spacy_model()
 
-def get_top_confidence_category(text):
-    """
-    Realiza a predição das categorias com base no texto fornecido, 
-    selecionando a categoria com maior nível de confiança ('Confidence')
-    gerada pelo modelo. Este método destina-se a identificar a 
-    categoria mais provável para apresentação no aplicativo web Streamlit.
+# Function to clean and lemmatize text
+def clean_and_lemmatize(text):
+    doc = nlp(text)
+    tokens = []
+    for token in doc:
+        if token.lemma_ not in STOP_WORDS and token.lemma_.isalpha():
+            tokens.append(token.lemma_.lower())
+    return " ".join(tokens)
 
-    Parâmetros:
-        text (str): Texto que será analisado pelo modelo.
-
-    Retorna:
-        str: O label da categoria com maior confiança prevista pelo modelo.
-    """
-
-    # Make prediction
-    prediction = predict(text)
-
-    # Find the index of the highest score
-    max_score_index = prediction['scores'].index(max(prediction['scores']))
-
-    # Use the index to find the corresponding class (Categoria)
-    categoria = prediction['classes'][max_score_index]
-
-    return categoria
-
-
-# Streamlit app layout
-st.title("Cloud Topic Modeling App")
-
-# Text input
-user_input = st.text_area("Escreva abaixo o texto do chamado para ser classificado", "")
-
-if st.button('Predict'):
-    if user_input:  # Check if there is an input
-        predicted_category = get_top_confidence_category(user_input)
-        st.markdown(f"### Categoria predita:")
-        
-        # Display the category in green
-        st.markdown(f"<h1 style='color: green;'>{predicted_category}</h1>", unsafe_allow_html=True)
-    else:
-        st.write("Por favor, insira o texto do chamado para ser classificado.")
-
-
-
-st.empty()
-st.empty()
-st.empty()
-
-
-
-# Custom function to display text examples in styled bubbles
 def display_example(text, background_color="#f0f2f6", text_color="#333"):
     """
     Display the example text in a styled bubble.
@@ -68,18 +29,46 @@ def display_example(text, background_color="#f0f2f6", text_color="#333"):
     """
     st.markdown(html, unsafe_allow_html=True)
 
+# Streamlit app layout
+st.title("Cloud Topic Modeling App")
+
+# Text input
+user_input = st.text_area("Escreva abaixo o texto do chamado para ser classificado", "")
+
+if st.button('Classificar texto'):
+    if user_input:  # Check if there is an input
+        lemmatized_user_input = clean_and_lemmatize(user_input)
+        predicted_category = get_top_confidence_category(lemmatized_user_input)
+        st.markdown(f"### Categoria predita:")
+        
+        # Display the category in green
+        st.markdown(f"<h1 style='color: green;'>{predicted_category}</h1>", unsafe_allow_html=True)
+    else:
+        st.write("Por favor, insira o texto do chamado para ser classificado.")
+
 # Sidebar for category selection
 st.sidebar.header("Precisa de ajuda?")
-category = st.sidebar.selectbox("Categoria", ["Escolha uma Categoria", "Hipotecas / Empréstimos", "Cartão de crédito / Cartão pré-pago", "Serviços de conta bancária"])
+category = st.sidebar.selectbox(
+    "Categorias exemplo",
+    ["Escolha uma Categoria",
+    "Hipotecas / Empréstimos",
+    "Roubo / Relatório de disputa",
+    "Serviços de conta bancária"
+    ]
+)
 
 # Example texts mapped to categories
 example_texts = {
-    "Hipotecas / Empréstimos": "Estou tentando refinanciar minha hipoteca há meses, mas não consigo obter uma taxa de juros satisfatória. Preciso de orientação sobre como proceder para conseguir uma melhor oferta.",
-    "Cartão de crédito / Cartão pré-pago": "Meu cartão de crédito foi clonado e observei transações não autorizadas. Como posso contestar essas cobranças e obter um novo cartão com segurança reforçada?",
-    "Serviços de conta bancária": "Houve um problema com a transferência que tentei fazer para outra conta. O valor foi debitado da minha conta, mas o destinatário não recebeu o dinheiro. Preciso de ajuda urgente para resolver essa questão."
+    "Hipotecas / Empréstimos": "Estou tentando refinanciar minha hipoteca há meses, mas não \
+        consigo obter uma taxa de juros satisfatória. Preciso de orientação sobre como proceder \
+            para conseguir uma melhor oferta.",
+    "Roubo / Relatório de disputa": "Meu cartão de crédito foi clonado e observei transações \
+        não autorizadas. Como posso contestar essas cobranças e obter um novo cartão com segurança \
+            reforçada?",
+    "Serviços de conta bancária": "Houve um problema com a transferência que tentei fazer para outra \
+        conta. O valor foi debitado da minha conta, mas o destinatário não recebeu o dinheiro. \
+            Preciso de ajuda urgente para resolver essa questão."
 }
-
-
 
 # Main area: Displaying the example based on selection
 st.header("Exemplo de texto para ser Predito")
@@ -88,4 +77,3 @@ if category and category != "Escolha uma Categoria":
     display_example(example_texts[category])
 else:
     st.write("Seleciona uma Categoria de Chamados para ver o texto de exemplo.")
-    st.empty()
