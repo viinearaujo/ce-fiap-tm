80f1636cd82f5920ff1fb3d351f77fa6a4f25ee4
Vinicius Araujo <araujoviine@gmail.com>
2024-03-24 13:35:56 -0300

    wip - adding meaningful comments and explanation

--- gcp.py
+++ gcp.py
@@ -4,23 +4,31 @@ import base64
 import json
 
 
-
+# GCP SERVICE ACCOUNT UNIQUE ID 
 SAC_UNIQUEID = "115532493627132210234"
+
+# GCP ENDPOINT ID
 ENDPOINT_ID = "6120385296526737408"
 
 
-
 def predict(text):
+    """
+    
+    """
 
-    teste = [
-        {   # Hipotecas / Empréstimos (Mortgages / Loans)
+
+    # Cria dado para ser analisado pelo modelo treinado utilizando o servico GCP Vertex AI
+    # Como o dataset de treino utiliza apenas a coluna 'descricao_reclamacao_processed'
+    # para classificar o texto com uma Categoria de chamado, apenas este dado e fornecido.
+    data = [
+        {   
             "descricao_reclamacao_processed": 
             text
         },
     ]   
 
 
-    # Load the encoded string from the new text file
+    # Read text file with the encoded authentication info
     with open('encoded-auth.txt', 'r') as encoded_file:
         encoded_string = encoded_file.read()
 
@@ -34,43 +42,32 @@ def predict(text):
     )
 
     aiplatform.init(
-        # your Google Cloud Project ID or number
-        # environment default used is not set
+        # Google Cloud Project ID
         project='topic-modelling-fiap-417601',
 
-        # the Vertex AI region you will use
-        # defaults to us-central1
+        # Vertex AI region
         location='us-central1',
 
         # Google Cloud Storage bucket in same region as location
-        # used to stage artifacts
+        # used to stage artifacts (artifacts = processed_file.csv)
+        # processed_file.csv --> Contem dados do dataset fornecido
+        # pelo professor (vide output do notebook preprocess_dataset.ipynb)
         staging_bucket='gs://tm-fiap-bucket',
 
         # custom google.auth.credentials.Credentials
-        # environment default credentials used if not set
         credentials=my_credentials,
 
-        # customer managed encryption key resource name
-        # will be applied to all Vertex AI resources if set
+        # GCP Service Account Unique ID
         encryption_spec_key_name=SAC_UNIQUEID
     )
 
+    # Instantiate endpoint providing Endpoint Unique ID
     endpoint = aiplatform.Endpoint(ENDPOINT_ID)
 
-    # Make the prediction
-    response = endpoint.predict(instances=teste)
+    # Make the prediction passing list of data (of a single value)
+    response = endpoint.predict(instances=data)
 
     # Process the response
     prediction = response.predictions[0]
-    
-        # Extract classes and scores from the prediction
-    predicted_classes = prediction['classes']
-    predicted_scores = prediction['scores']
 
-    # Print each class with its corresponding score
-    print("Prediction results:")
-    for predicted_class, score in zip(predicted_classes, predicted_scores):
-        print(f"{predicted_class}: {score:.2f}")
-        print("-------------------")
-    
     return prediction
